629d792423e6465eddf01eb77053a06efc637be4
Dragin <dragin@dragin.xyz>
2025-01-15 13:22:57 -0600

    Add replying

--- confessions.rb
+++ confessions.rb
@@ -16,6 +16,16 @@ bot.register_application_command(:confess, 'Submit a confession!', server_id: EN
 #  cmd.attachment('attachment', 'Optional file to attach with the confession', required: false)
 end
 
+# Constants
+
+CONFESS_BUTTON = {label: 'Confess', style: :primary, custom_id: 'confess_button'}
+REPLY_BUTTON = {label: 'Reply', style: :secondary, custom_id: 'reply_button'}
+REPLY_CONTINUE_BUTTON = {label: 'Reply', style: :secondary, custom_id: 'reply_continue_button'}
+CONFESSION_SUBMIT_MESSAGE = {content: 'Your confession has been submitted!', ephemeral: true}
+REPLY_SUBMIT_MESSAGE = {content: 'Your reply has been posted! (If you pressed the reply button on the main message and a thread exists, no it hasn\'t. I\'m working on fixing that :P)', ephemeral: true}
+MODAL_CONFESSION_TEXT_INPUT = {label: 'Confession message', style: :paragraph, custom_id: 'confession', required: true}
+MODAL_REPLY_TEXT_INPUT = {label: 'Reply message', style: :paragraph, custom_id: 'reply', required: true}
+
 # Methods
 
 def send_confession_message(channel, message)
@@ -23,8 +33,30 @@ def send_confession_message(channel, message)
     embed.title = 'Anonymous Confession'
     embed.description = message
     view.row do |r|
-      r.button(label: 'Confess', style: :primary, custom_id: 'confess_button:912')
-      r.button(label: 'Reply', style: :secondary, custom_id: 'reply_button:432')
+      r.button(**CONFESS_BUTTON)
+      r.button(**REPLY_BUTTON)
+    end
+  end
+end
+
+def send_confession_reply(channel, reply_message, confession = nil, create_thread_if_not_nil = true)
+  reference = nil
+  if confession != nil
+    if create_thread_if_not_nil
+      begin
+        channel = channel.start_thread('Confession Replies', 10080, message: confession)
+      rescue Discordrb::Errors::UnknownError
+        return
+      end
+    else
+      reference = confession
+    end
+  end
+  channel.send_embed('', nil, nil, false, nil, reference) do |embed, view|
+    embed.title = 'Anonymous Reply'
+    embed.description = reply_message
+    view.row do |r|
+      r.button(**REPLY_CONTINUE_BUTTON)
     end
   end
 end
@@ -33,23 +65,39 @@ end
 
 bot.application_command(:confess) do |event|
   send_confession_message(event.channel, event.options['confession'])
-  event.respond(content: 'Your confession has been submitted!', ephemeral: true)
+  event.respond(**CONFESSION_SUBMIT_MESSAGE)
 end
 
 # Interaction handlers
 
 # Buttons
 
-bot.button(custom_id: /^confess_button:/) do |event|
-  event.show_modal(title: 'test', custom_id: 'confess_modal:55') do |modal|
+bot.button(custom_id: 'confess_button') do |event|
+  event.show_modal(title: 'Submitting a confession', custom_id: 'confess_modal') do |modal|
 #    def text_input(style:, custom_id:, label: nil, min_length: nil, max_length: nil, required: nil, value: nil, placeholder: nil)
     modal.row do |r|
-      r.text_input(label: 'Text of the confession', style: :paragraph, custom_id: 'confession', required: true)
+      r.text_input(**MODAL_CONFESSION_TEXT_INPUT)
     end
   end
 end
 
-bot.button(custom_id: /^test_button:/) do |event|
+bot.button(custom_id: 'reply_button') do |event|
+  event.show_modal(title: 'Replying to a confession', custom_id: 'reply_modal') do |modal|
+    modal.row do |r|
+      r.text_input(**MODAL_REPLY_TEXT_INPUT)
+    end
+  end
+end
+
+bot.button(custom_id: 'reply_continue_button') do |event|
+  event.show_modal(title: 'Replying to a confession', custom_id: 'reply_continue_modal') do |modal|
+    modal.row do |r|
+      r.text_input(**MODAL_REPLY_TEXT_INPUT)
+    end
+  end
+end
+
+bot.button(custom_id: 'test_button') do |event|
   num = event.interaction.button.custom_id.split(':')[1].to_i
 
   event.update_message(content: num.to_s) do |_, view|
@@ -62,9 +110,18 @@ end
 
 # Modals
 
-bot.modal_submit(custom_id: /^confess_modal:/) do |event|
+bot.modal_submit(custom_id: 'confess_modal') do |event|
   send_confession_message(event.channel, event.value('confession'))
-  event.respond(content: 'Your confession has been submitted!', ephemeral: true)
+  event.respond(**CONFESSION_SUBMIT_MESSAGE)
 end
 
+bot.modal_submit(custom_id: 'reply_modal') do |event|
+  send_confession_reply(event.channel, event.value('reply'), event.message)
+  event.respond(**REPLY_SUBMIT_MESSAGE)
+end
+
+bot.modal_submit(custom_id: 'reply_continue_modal') do |event|
+  send_confession_reply(event.channel, event.value('reply'), event.message, false)
+  event.respond(**REPLY_SUBMIT_MESSAGE)
+end
 bot.run
